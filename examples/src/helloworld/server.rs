8d489b30d09022922ad0a5781152e5d031003ca8
Ihor Horobets <ihor@horobets.me>
2020-09-02 20:17:07 +0300

    Try to make work with async-std.

--- examples/src/helloworld/server.rs
+++ examples/src/helloworld/server.rs
@@ -1,4 +1,8 @@
-use tonic::{transport::Server, Request, Response, Status};
+use async_std::net::TcpListener;
+use async_std::task;
+// use hyper::service::{make_service_fn, service_fn};
+use tonic::{ Request, Response, Status};
+use hyper::{Server};
 
 use hello_world::greeter_server::{Greeter, GreeterServer};
 use hello_world::{HelloReply, HelloRequest};
@@ -16,8 +20,6 @@ impl Greeter for MyGreeter {
         &self,
         request: Request<HelloRequest>,
     ) -> Result<Response<HelloReply>, Status> {
-        println!("Got a request from {:?}", request.remote_addr());
-
         let reply = hello_world::HelloReply {
             message: format!("Hello {}!", request.into_inner().name),
         };
@@ -25,17 +27,86 @@ impl Greeter for MyGreeter {
     }
 }
 
-#[tokio::main]
-async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let addr = "[::1]:50051".parse().unwrap();
+pub mod compat {
+    use std::pin::Pin;
+    use std::task::{Context, Poll};
+
+    use async_std::io;
+    use async_std::net::{TcpListener, TcpStream};
+    use async_std::prelude::*;
+    use async_std::task;
+
+    #[derive(Clone)]
+    pub struct HyperExecutor;
+
+    impl<F> hyper::rt::Executor<F> for HyperExecutor
+    where
+        F: Future + Send + 'static,
+        F::Output: Send + 'static,
+    {
+        fn execute(&self, fut: F) {
+            task::spawn(fut);
+        }
+    }
+
+    pub struct HyperListener(pub TcpListener);
+
+    impl hyper::server::accept::Accept for HyperListener {
+        type Conn = HyperStream;
+        type Error = io::Error;
+
+        fn poll_accept(
+            mut self: Pin<&mut Self>,
+            cx: &mut Context,
+        ) -> Poll<Option<Result<Self::Conn, Self::Error>>> {
+            let stream = task::ready!(Pin::new(&mut self.0.incoming()).poll_next(cx)).unwrap()?;
+            Poll::Ready(Some(Ok(HyperStream(stream))))
+        }
+    }
+
+    pub struct HyperStream(pub TcpStream);
+
+    impl tokio::io::AsyncRead for HyperStream {
+        fn poll_read(
+            mut self: Pin<&mut Self>,
+            cx: &mut Context,
+            buf: &mut [u8],
+        ) -> Poll<io::Result<usize>> {
+            Pin::new(&mut self.0).poll_read(cx, buf)
+        }
+    }
+
+    impl tokio::io::AsyncWrite for HyperStream {
+        fn poll_write(
+            mut self: Pin<&mut Self>,
+            cx: &mut Context,
+            buf: &[u8],
+        ) -> Poll<io::Result<usize>> {
+            Pin::new(&mut self.0).poll_write(cx, buf)
+        }
+
+        fn poll_flush(mut self: Pin<&mut Self>, cx: &mut Context) -> Poll<io::Result<()>> {
+            Pin::new(&mut self.0).poll_flush(cx)
+        }
+
+        fn poll_shutdown(mut self: Pin<&mut Self>, cx: &mut Context) -> Poll<io::Result<()>> {
+            Pin::new(&mut self.0).poll_close(cx)
+        }
+    }
+}
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    task::block_on(async {
     let greeter = MyGreeter::default();
 
-    println!("GreeterServer listening on {}", addr);
+    let addr = "[::1]:50051".parse().unwrap();
+    let listener = TcpListener::bind(addr).await?;
 
-    Server::builder()
-        .add_service(GreeterServer::new(greeter))
+    println!("GreeterServer listening on {}", addr);
+    let server = Server::builder(compat::HyperListener(listener)).executor(compat::HyperExecutor);
+    server.add_service(GreeterServer::new(greeter))
         .serve(addr)
         .await?;
 
     Ok(())
+    })
 }
